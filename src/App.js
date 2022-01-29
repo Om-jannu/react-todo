78ca21367d38c129c74cc23cd3e0efcdc4840010
Om_jannu <omjannu2002@gmail.com>
2022-01-29 19:17:20 +0530

    started react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
+import react, { useState } from 'react';
 function App() {
+  const [inputlist,setinputlist] = useState("");
+  const [items,setitems] = useState([]);
+
+
+  const itemevent=(event) =>{
+    setinputlist(event.target.value);
+  };
+
+  const addlisthandler=()=>{
+    setitems((olditems)=>{
+      return [...olditems,inputlist];
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="maindiv">
+      <div className="container">
+        <h2>TODO List</h2>
+        <br />
+        <input type="text" placeholder='Add a ToDo' onChange={itemevent}/>
+        <button onClick={addlisthandler}>+</button>
+        <br />
+        <ol>
+          {/* <li>{inputlist}</li> */
+          items.map((itemval)=>{
+            return <li>{itemval}</li>
+          })
+          }
+        </ol>
+      </div>
     </div>
+    </>
   );
 }
 
